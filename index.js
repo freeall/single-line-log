b8ea73f7d1e6f67d1167a99953b258838f11d75e
freeall <freeall@gmail.com>
2016-02-27 06:33:23 +0100

    Moved test case to test.js

--- index.js
+++ index.js
@@ -49,18 +49,3 @@ module.exports = function(stream) {
 
 module.exports.stdout = module.exports(process.stdout);
 module.exports.stderr = module.exports(process.stderr);
-
-if (require.main !== module) return;
-
-var log = module.exports.stdout;
-
-var i=0;
-setInterval(function() {
-	i++;
-	var s = 'line 1 - '+Math.random();
-	if (i < 10) s += ' - '+Math.random();
-	s += '\nline 2 - '+Math.random();
-	if (i<20) s += '\nline 3 - '+Math.random()+'\nline 4 - '+Math.random();
-
-	log(s);
-}, 200);
